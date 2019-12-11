6b4e7630499ad316e11eba77579a307427a9f8ff
gealcan <gealcan.neto@gmail.com>
2019-12-11 18:51:32 -0300

    Primeiro Commit

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+        <i className=" fa fa-desktop text-dark"></i> Dev by
+            <strong> Guid<span className="text-danger">@</span>nce</strong>
+        </span>
+    </footer>
---
+++ crud/frontend/src/main/App.jsx
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+
+export default props => 
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main icon="home" title="Inicio"
+            subtitle="Segundo Projeto do capítulo de React.">
+            <div className='display-4'>Bem Vindo!</div>
+            <hr />
+            <p className="mb-0">Sistema para exemplificar a construção
+             de um cadastro desenvolvido em React</p>
+
+        </Main>
+        <Footer />
+    </div>
